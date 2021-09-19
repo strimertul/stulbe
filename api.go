860b949d447910136c978b496890d44f8a08287e
Ash Keel <ash@nebula.cafe>
2021-09-19 22:29:41 +0200

    Forgot the auth header woops

--- api.go
+++ api.go
@@ -52,7 +52,7 @@ func cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
 			return
